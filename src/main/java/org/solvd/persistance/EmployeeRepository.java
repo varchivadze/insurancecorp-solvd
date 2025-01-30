96584515369f35e95dbeb0bf46c84becc3d9c491
Vladyslav Archivadze <svatvlad1993@gmail.com>
2025-01-30 21:53:11 +0100

    DAO final

--- src/main/java/org/solvd/persistance/EmployeeRepository.java
+++ src/main/java/org/solvd/persistance/EmployeeRepository.java
@@ -5,5 +5,6 @@ import org.solvd.domain.staff.Employee;
 import java.sql.SQLException;
 
 public interface EmployeeRepository extends CrudRepository<Employee> {
+
     void create(Employee address, Long companyId) throws SQLException;
 }
